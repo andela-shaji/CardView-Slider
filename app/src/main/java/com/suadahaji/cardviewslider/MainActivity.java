743268f0fd67446324460d8451ac022700a7fa9d
“Suada <suada.haji@andela.com>
2016-10-15 13:33:47 +0300

    Refactor code

--- app/src/main/java/com/suadahaji/cardviewslider/MainActivity.java
+++ app/src/main/java/com/suadahaji/cardviewslider/MainActivity.java
@@ -1,13 +1,36 @@
 package com.suadahaji.cardviewslider;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.suadahaji.designcardview.CardViewSliderActivity;
+import com.suadahaji.designcardview.views.CardDesignCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends CardViewSliderActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    showActionBar();
+
+
+    CardDesignCard cardDesignCard1 = new CardDesignCard("Popular", R.drawable.captain);
+    CardDesignCard cardDesignCard2 = new CardDesignCard("Top Rated", R.drawable.image_one);
+    CardDesignCard cardDesignCard3 = new CardDesignCard("Upcoming", R.drawable.inferno);
+    CardDesignCard cardDesignCard4 = new CardDesignCard("Now Playing", R.drawable.image_two);
+
+    List<CardDesignCard> cardList = new ArrayList<>();
+    cardList.add(cardDesignCard1);
+    cardList.add(cardDesignCard2);
+    cardList.add(cardDesignCard3);
+    cardList.add(cardDesignCard4);
+
+    Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+    setFont(face);
+
+    setCardViewPages(cardList);
   }
 }
